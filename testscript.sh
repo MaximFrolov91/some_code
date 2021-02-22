82c9d321202f32d3096422738c057102c6fa850f
Maksym Frolov <frolov.m.a.1991@gmail.com>
2021-02-22 10:55:04 +0200

    Add some text to echo

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE+$NUMBERTWO))
 echo $NUMBER
 echo "this is complete"
-echo "test work with github webhooks"
+echo "test work with github webhooks after push statement"
